d25f9659858490f1a219b4eb1b90133008fa2f24
Dennis Smith <114948160+Dennis-Smith414@users.noreply.github.com>
2024-04-30 21:43:45 -0500

    created file reading in main, added execution class

---
+++ src/main/java/org/example/Executor.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Executor {
+    private int[] registers;
+    private int[] memory;
+    public Executor(int[] registers, int[] memory) {
+        this.registers = registers;
+        this.memory = memory;
+    }
+    //determine which instruction to execute
+    //support all the required instuctions r, l, lw, ect...
+    //handle syscall
+    //use the classes
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,73 @@
 package org.example;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    //init the MIPS processor
+    private static final int REG_NUM = 32;
+    private static final int MEM_SIZE = 0x1000;
+
+    private int[] registers = new int[REG_NUM];
+    private int[] mem = new int[MEM_SIZE];
+    private int programCounter;
+
+
   public static void main(String[] args) {
-    String result = Controller.process(args);
-    System.out.println(result);
+    if (args.length != 2) {
+      System.out.println("Two arugements are needed!");
+      System.exit(1);
+    }
+
+    Main sim = new Main();
+    sim.loadText(args[0]);
+    sim.loadData(args[1]);
+    sim.run();
+  }
+
+  private void loadText(String text) {
+    try (BufferedReader read = new BufferedReader(new FileReader(text))) {
+      String line;
+      int address = 0x00400000;
+      while ((line = read.readLine()) != null) {
+        int instruction = Integer.parseInt(line, 16);
+        mem[address] = instruction;
+        address += 4;
+      }
+      programCounter = 0x00400000;
+    } catch (IOException e) {
+        throw new RuntimeException();
+    }
+  }
+
+  private void loadData(String data) {
+    try (BufferedReader read = new BufferedReader(new FileReader(data))) {
+      String line;
+      int address = 0x10010000;
+      while ((line = read.readLine()) != null) {
+        int dataActual = Integer.parseInt(line, 16);
+        mem[address] = dataActual;
+        address += 4;
+      }
+      programCounter = 0x00400000;
+    } catch (IOException e) {
+      throw new RuntimeException();
+    }
+
+
+  }
+
+  private void run() {
+
+
+  }
+
+
+    //Im not sure if these need to be commmented out but its one more thing to keep track of.
+//    String result = Controller.process(args);
+//    System.out.println(result);
+
   }
 }
