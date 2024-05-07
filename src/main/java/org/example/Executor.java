2463ccf6e9cf68c03252392b6a7f3cb37cc2ccbc
Dennis Smith <114948160+Dennis-Smith414@users.noreply.github.com>
2024-05-06 23:11:35 -0500

    almost done with Executor class, need to finish the sys call and get main running

--- src/main/java/org/example/Executor.java
+++ src/main/java/org/example/Executor.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Enumeration;
+
 public class Executor {
     private int[] registers;
     private int[] memory;
@@ -33,19 +35,86 @@ public class Executor {
     }
 
     private void doRType(int instruction) {
+        String funct = Integer.toBinaryString(instruction & 0x3F);
+        funct = String.format("%06d", Integer.parseInt(funct));
+        RegisterType r = new RegisterType(funct);
+        r.toDisassembled();
+
+        int rd = Integer.parseInt(r.rd, 16);
+        int rs = Integer.parseInt(r.rs, 16);
+        int rt = Integer.parseInt(r.rt, 16);
+
+        switch (funct) {
+            case "100000":
+                registers[rd] = registers[rs] + registers[rt];
+                break;
+            case "100010":
+                registers[rd] = registers[rs] + registers[rt];
+                break;
+            case "100100":
+                registers[rd] = registers[rs] + registers[rt];
+                break;
+            case "100101":
+                registers[rd] = registers[rs] + registers[rt];
+                break;
+            case "101010":
+                registers[rd] = registers[rs] + registers[rt];
+                break;
+            default:
+                System.out.println("Invalid R type");
+        }
+
 
     }
     private void doLw(int instruction) {
+        ImmediateType I = new ImmediateType(Integer.toBinaryString(instruction >>> 26));
+        I.toDisassembled();
+        int rt = Integer.parseInt(I.rt, 16);
+        int rs = Integer.parseInt(I.base, 16);
+        int offset = Integer.parseInt(I.offset, 16);
+        int address = registers[rs] + offset;
+        registers[rt] = memory[address];
 
     }
     private void doSw(int instruction) {
-
+        ImmediateType I = new ImmediateType(Integer.toBinaryString(instruction >>> 26));
+        I.toDisassembled();
+        int rt = Integer.parseInt(I.rt, 16);
+        int rs = Integer.parseInt(I.base, 16);
+        int offset = Integer.parseInt(I.offset, 16);
+        int address = registers[rs] + offset;
+        memory[address] = registers[rt];
     }
     private void doLui(int instruction) {
-
+        ImmediateType iType = new ImmediateType(Integer.toBinaryString(instruction >>> 26));
+        iType.toDisassembled();
+        int rt = Integer.parseInt(iType.rt, 16);
+        int immediate = Integer.parseInt(iType.offset, 16);
+        registers[rt] = immediate << 16;
     }
     private void doSyscall(int instruction) {
+        Syscall sys = new Syscall();
+        String[] sysInstruction = sysHelper(instruction);
+        sys.toMachine(sysInstruction);
 
+        //figure out the rest
+
+    }
+
+    private String[] sysHelper(int instruction) {
+        if (instruction == 0) return new String[] {"0"};
+        StringBuilder convert = new StringBuilder();
+        while (instruction > 0) {
+            convert.append(instruction % 10);
+            instruction /= 10;
+        }
+        String convertString = convert.toString();
+        int num = convertString.length();
+        String[] sysConvert = new String[num];
+        for (int i = 0; i < num; i++) {
+            sysConvert[i] = String.valueOf(convertString.charAt(i));
+        }
+        return sysConvert;
     }
 
     //support all the required instuctions r, l, lw, ect...
